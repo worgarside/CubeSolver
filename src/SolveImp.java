f0afcc17dd199742e423610a59e1f8d8507f81d3
Will Garside <worgarside@gmail.com>
2017-06-13 01:10:18 +0100

    Initialized solving implementation file

--- src/SolveImp.java
+++ src/SolveImp.java
@@ -1,1 +1,8 @@
-public class SolveImp
+public class SolveImp {
+    public static void main(String[] args) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screen = toolkit.getScreenSize();
+        CubeGUI inputWindow = new CubeGUI();
+        inputWindow.setLocation((screen.width - inputWindow.getWidth()) / 2, (screen.height - inputWindow.getHeight()) / 2);
+    }
+}
