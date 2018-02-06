deadc720ecb39f60d039579cd026a736ed975a6e
Will Garside <worgarside@gmail.com>
2018-02-06 17:54:47 +0000

    Dynamic creation of positions started - need to use actual moves though

--- Codebase/position_generator.py
+++ Codebase/position_generator.py
@@ -1,16 +1,37 @@
 from linked_list import LinkedList
 from cube.move_class import Move as MOVE
-import datetime
+from cube.moves import *
+from cube.cube_class import Cube
+
+DEPTH_LIMIT = 5
+
+positions = {} # depth: set(position)
 
 def generate_positions(cube, group):
+    pos_list = LinkedList()
+
     depth = 0
     id = 0
-    pos_list = LinkedList()
-    with open(datetime.datetime.now().strftime("%Y-%m-%d %H%M") + ".csv", "w+") as f:
-        while depth < 500000:
-            pos_list.push(id, cube.pos, MOVE.U, depth)
-            depth += 1
-            print(depth, end=' ')
-            f.write(str(id) + ',' + cube.pos + ',' + str(MOVE.U) + ',' + str(depth) + "\n")
-    f.close()
-    # print(pos_list)
+
+    pos_list.push(id, cube.pos, MOVE.NONE, depth)
+    positions[depth] = {cube.pos}
+
+    while depth < DEPTH_LIMIT:
+        positions[depth+1] = set()
+        for p in positions[depth]:
+            for m in group:
+                c = Cube(p)
+                u(c)
+                positions[depth + 1].add(c.pos)
+
+        depth += 1
+
+
+
+
+
+        # pos_list.push(id, cube.pos, MOVE.U, depth)
+        # depth += 1
+        # print(depth, end=' ')
+
+    print(positions)
