a4ca75a951563e1c0f4a46129af2f9b4a2237a69
Will Garside <worgarside@gmail.com>
2018-02-07 13:31:41 +0000

    Duplication & leaf checking

--- Codebase/main.py
+++ Codebase/main.py
@@ -9,6 +9,8 @@ GROUP_TWO = [MOVE.U2, MOVE.D2, MOVE.L, MOVE.R, MOVE.F2, MOVE.B2]
 GROUP_ONE = [MOVE.U2, MOVE.D2, MOVE.L, MOVE.R, MOVE.F, MOVE.B]
 GROUP_ZERO = [MOVE.U, MOVE.D, MOVE.L, MOVE.R, MOVE.F, MOVE.B]
 
+GROUP_TEST = [MOVE.U, MOVE.R]
+
 start = int(round(time.time() * 1000))
 pos_vals = generate_positions(Cube(), GROUP_THREE)
 end = int(round(time.time() * 1000))
@@ -16,7 +18,7 @@ end = int(round(time.time() * 1000))
 total = (end - start)/1000
 print("Total Time: " + str(total))
 
-with open(datetime.datetime.now().strftime("%Y-%m-%d %H%M") + ".csv", "w+") as f:
+with open("Positions/" + datetime.datetime.now().strftime("%Y-%m-%d %H%M") + ".csv", "w+") as f:
     for p in pos_vals:
         print(len(p))
         for q in p:
--- Codebase/position_generator.py
+++ Codebase/position_generator.py
@@ -1,11 +1,13 @@
 from cube.moves import *
 from cube.cube_class import Cube
-from position_class import Position # (id, position, depth, parent, parent_move, leaf)
+from position_class import Position # (id, position, depth, parent_id, parent_move, leaf)
 
-DEPTH_LIMIT = 5
+DEPTH_LIMIT = 4
 
 def generate_positions(cube, group):
+    move_count = len(group)
     positions = {}  # depth: set(position)
+    position_set = set()
     depth = 0
     id = 0
 
@@ -14,7 +16,9 @@ def generate_positions(cube, group):
     while depth < DEPTH_LIMIT:
         positions[depth+1] = set()
         for p in positions[depth]:
-            p_id = p.id
+            prev_count = 0
+            if p.leaf == True:
+                print('LEAF')
             for m in group:
                 c = Cube(p.position)
                 dyn_move(c, m)
@@ -23,8 +27,18 @@ def generate_positions(cube, group):
                 symmetrical = False
 
                 if not symmetrical:
-                    id += 1
-                    positions[depth + 1].add(Position(id, c.position, depth, p_id, m, False))
+                    if c.position not in position_set:
+                        id += 1
+                        # write all data to file / add to database
+                        positions[depth + 1].add(Position(id, c.position, depth, p.id, m, False))
+
+                        position_set.add(c.position)
+                    else:
+                        prev_count += 1
+
+            # if all m are in position_set, then p.leaf = True
+            if prev_count == 6:
+                p.leaf = True
 
         depth += 1
         print(depth)
